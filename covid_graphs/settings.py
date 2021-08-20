f917c308a4f7fa785cae6786d15dac3de0c480d4
Emil Elgaard <shivan@shivan.dk>
2021-08-20 14:43:24 -0400

    update requirements.txt and allowed hosts

--- covid_graphs/settings.py
+++ covid_graphs/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-*n!68te)f-c1mwiie%2^&t$amqfxfdic%gse50uxz0*t*oj-=e
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
